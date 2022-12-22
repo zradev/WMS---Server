750de5565c3993f8999ae15dabbff214db398f0e
zradev <zlatkoradevv@gmail.com>
2022-12-23 00:16:10 +0200

    Add Product Model

---
+++ models/product.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  images: { type: [String], required: true },
+  buyingPrice: { type: Number, required: true },
+  sellingPrice: { type: Number, required: true },
+  quantity: { type: Number, default: 1 },
+  category: { type: String, required: true },
+});
+
+Product = mongoose.model("product", productSchema);
+
+module.exports = { Product };
