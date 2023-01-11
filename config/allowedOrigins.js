a94aa3fc6e341a401c40b597309abfc8c76f537a
zradev <zlatkoradevv@gmail.com>
2023-01-11 16:43:47 +0200

    commit

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,7 @@
-const allowedOrigins = ["http://localhost:3000", "https://wms-react.web.app/"];
+const allowedOrigins = [
+	"http://localhost:3000",
+	"https://wms-react.web.app",
+	"https://wms-react.firebaseapp.com",
+];
 
 module.exports = allowedOrigins;
